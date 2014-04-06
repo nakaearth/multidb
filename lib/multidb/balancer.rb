1ce493aff52da73864439641331c696181890a95
nakamura shinichirou <naka5313@gmail.com>
2014-04-06 22:37:41 +0900

    update balencer.rb

--- lib/multidb/balancer.rb
+++ lib/multidb/balancer.rb
@@ -1,5 +1,23 @@
 module Multidb
   class Balencer
+    def  initialize(configuration)
+      @configs = configuration
+      if @configs
+        @configs.each_pair do |name , config|
+          @connections[name].push(Multidb::Connection.new(config))
+        end
+      end
+    end
 
+   def get(name, &block)
+     @connection = @connections[name]  
+      block_given? ? yield(@connection) : @connection 
+   end
+
+    def use(name, &block)
+      if block_given?
+
+      end
+    end
   end
 end
--- lib/multidb/configuration.rb
+++ lib/multidb/configuration.rb
@@ -8,20 +8,18 @@ module Multidb
 
     def initialize(default_adapter)
       #      @default_pool = ActiveRecord::Base.connection_pool
-      yaml = Pathname.new("config/database.yml")
       @default_adapter = default_adapter
 
+      yaml = Pathname.new("config/database.yml")
       @raw_configuration = if yaml.exist?
-                 require "yaml"
-                 require "erb"
-                 YAML.load(ERB.new(yaml.read).result) || {}
-               elsif ENV['DATABASE_URL']
-                 # Value from ENV['DATABASE_URL'] is set to default database connection
-                 # by Active Record.
-                 {}
-               else
-                 raise "Could not load database configuration. No such file - #{yaml}"
-               end
+                             require "yaml"
+                             require "erb"
+                             YAML.load(ERB.new(yaml.read).result) || {}
+                           elsif ENV['DATABASE_URL']
+                             {}
+                           else
+                             raise "Could not load database configuration. No such file - #{yaml}"
+                           end
     end 
   end
 end
