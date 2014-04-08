8f991845ee77e824f3a636a106bd8bafa8005ceb
nakamura shinichirou <naka5313@gmail.com>
2014-04-09 00:13:02 +0900

    update balancer.rb

--- lib/multidb/balancer.rb
+++ lib/multidb/balancer.rb
@@ -4,20 +4,39 @@ module Multidb
       @configs = configuration
       if @configs
         @configs.each_pair do |name , config|
-          @connections[name].push(Multidb::Connection.new(config))
+          @connections[name.to_sym].push(Multidb::Connection.new(config))
         end
       end
     end
 
-   def get(name, &block)
-     @connection = @connections[name]  
+    def get(name, &block)
+      @connection = @connections[name]  
       block_given? ? yield(@connection) : @connection 
-   end
+    end
 
     def use(name, &block)
-      if block_given?
-
+      result = null
+      get(name) do |con|
+        if block_given?
+          con.connection do|connect|
+            previous_connect, Thread.current[:multidb_connect] = 
+              Thread.current[:multidb_connect], connect
+            begin
+              result = yield
+            ensure 
+              Thread.current[:multidb_connect] = previous_connect
+            end
+            result
+          end
+        else
+          result = Thread.current[:multidb_connect] = con.connection
+        end
       end
+      result
+    end
+
+    def current_connection
+      Thread.current[:multidb_connect]
     end
   end
 end
